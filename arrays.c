b2fda78677147cea4544af6d0670315281883823
BYTE BANDIT <cmekthiago@gmail.com>
2025-10-02 13:34:26 +0300

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main ()
+{
+  int age [3];
+
+  age[0] = 20;
+  age[1] = 50;
+  age[2] = 25;
+  age[3] = 68;
+
+
+  printf("%d\n",age[0]);
+  printf("%d\n",age[1]);
+  printf("%d\n",age[2]);
+  printf("%d\n",age[3]);
+
+  return 0;
+}
